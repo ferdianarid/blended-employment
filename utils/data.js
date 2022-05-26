8adfad0d1e6b05d440bf17282cda94bae0f35d15
Ferdian Ahmad R <ferdianahmadrozikin10@gmail.com>
2022-05-26 14:42:05 +0700

    feature(data): add example data

---
+++ utils/data.js
@@ -0,0 +1,34 @@
+export const Leslie = [{
+        id: 1,
+        title: "Hello IT Group"
+    },
+    {
+        id: 2,
+        title: "Bussiness Development"
+    },
+    {
+        id: 3,
+        title: "12/05/2022"
+    }
+]
+
+export const personList = [{
+        id: 1,
+        name: "Emma Quinell",
+        image: "/homepage/faces/faces1.png",
+        roles: "Senior Paid Search Strategist"
+    },
+    {
+        id: 2,
+        name: "Dave Piggot",
+        image: "/homepage/faces/faces2.png",
+        roles: "Interfuse Technologies"
+    }
+]
+
+export const InactiveCandidate = [{
+    id: 1,
+    image: "/homepage/faces/faces3.png",
+    name: "Peter Hooper",
+    data: ["Hot Doc", "Sales Director", "12/05/2022"]
+}]
